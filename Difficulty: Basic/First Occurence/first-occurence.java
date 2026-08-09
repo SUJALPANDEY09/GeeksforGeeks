84a488e05f22fa16a582ead89aee519e38de5866
SUJAL SANAT PANDEY <sujal69pandey@gmail.com>
2026-08-09 13:24:39 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/First Occurence/first-occurence.java
@@ -0,0 +1,27 @@
+class Solution {
+    int firstOccurence(String txt, String pat) {
+        // code here
+          int n = txt.length();
+        int m = pat.length();
+
+        if (m == 0)
+            return 0;
+
+        if (m > n)
+            return -1;
+
+        for (int i = 0; i <= n - m; i++) {
+
+            int j = 0;
+
+            while (j < m && txt.charAt(i + j) == pat.charAt(j)) {
+                j++;
+            }
+
+            if (j == m)
+                return i;
+        }
+
+        return -1;
+    }
+}
